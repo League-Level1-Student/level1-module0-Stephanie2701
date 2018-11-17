7872b04cd9356867beeae03af539e8da282c74f8
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-11-17 10:38:37 -0800

    gitignore My First Swing

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,31 +15,34 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
+		JFrame Frame= new JFrame();
 
 		//2. Set your JFrame object to be visible
+		Frame.setVisible(true);
 
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+		Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+		JPanel Panel= new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+		JLabel Label= new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+		Label.setText("Welcome!");
 		//8. Add the JPanel object to the JFrame
-
+		Frame.add(Panel);
 		//9. Add the JLabel object to the JPanel
-
+		Panel.add(Label);
 		//10. Pack your JFrame.
-
+		Frame.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
-		//13. Re-pack the JFrame object.
+		Label.setIcon(loadImage());
 		
+		//13. Re-pack the JFrame object.
+		Frame.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,9 +22,9 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                String L(https://shop.monrovia.com/media/catalog/product/5/5/551_2.jpg);
 		// 2. create a variable of type "Component" that will hold your image
-
+              
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
@@ -32,9 +32,9 @@ public class PhotoQuiz {
 		// 5. call the pack() method on the quiz window
 
 		// 6. ask a question that relates to the image
-
+             String P= JOptionPane.showInputDialog("what color are the flowers");
 		// 7. print "CORRECT" if the user gave the right answer
-
+                if()
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
