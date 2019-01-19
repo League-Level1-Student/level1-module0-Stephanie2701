8e10e20eb91f85386433c1b91f4cf57912e24c99
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-01-19 10:55:03 -0800

    cutenesstv

--- src/extra/cutnesstv.java
+++ src/extra/cutnesstv.java
@@ -2,6 +2,7 @@ package extra;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.URI;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,36 +10,54 @@ import javax.swing.JPanel;
 
 public class cutnesstv implements ActionListener { 
 	JFrame frame;
-	JPanel panel;
-	JButton button;
-	
-	public static void main(String[] args) {
-		
-		new cutnesstv().CreateUI();	
-	}
-	void CreateUI(){
-		button= new JButton();
-		frame = new JFrame();
-		frame.setVisible(true);
-		
-		panel =new JPanel();
-		frame.add(panel);
-		
-		button.addActionListener(this);
-		frame.pack();
-		
-	}
+    JPanel panel;
+    JButton pink;
+    JButton yellow;
+    JButton blue;
+    
+    
+    public static void main(String[] args) {
+        
+        new cutnesstv().CreateUI();    
+    }
+    void CreateUI(){
+        yellow= new JButton();
+        pink=new JButton();
+        blue=new JButton();
+        frame = new JFrame();
+        
+        
+        
+        panel =new JPanel();
+        frame.add(panel);
+        
+        panel.add(blue);
+        panel.add(pink);
+        panel.add(yellow);
+        
+        
+        blue.addActionListener(this);
+        pink.addActionListener(this);
+        yellow.addActionListener(this);
+        frame.pack();
+    frame.setVisible(true);
+        
+    }
 
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()== button) {
-			showDucks();
-		}
-		else {
-			showFrog();
-		}
-		
-	}
-
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()== blue) {
+            showDucks();
+        }
+        else if(e.getSource()==yellow){
+            showFrog();
+        }
+        else if(e.getSource()==pink) {
+            showFluffyUnicorns();
+        }
+        
+        
+        
+    }
 
 	void showDucks() {
 	     playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
